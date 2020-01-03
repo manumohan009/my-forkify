06bb2adce673f51506b5a4b2c753c85920202f78
Manu M <manu.m@nestgroup.net>
2020-01-03 19:53:29 +0530

    Initial commit, forkify app initial setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer:{
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader' 
+                }
+            }
+        ]
+    }
+    // mode: 'development'
+};
+
+// production mode 
+    // minification, treeshaking
+// development mode
+
+// In webpack we have 
+//     entry point 
+//     output
+//     loaders
+//     plugins
